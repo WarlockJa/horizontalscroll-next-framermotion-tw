cf62c9cd9d1120bf65f4d682f8ecd1df665f728b
warlockja <warlockja@gmail.com>
2023-10-26 13:44:38 +0300

    left/right and screen width adjustment

--- src/app/component/HorizontalScrollCarousel.tsx
+++ src/app/component/HorizontalScrollCarousel.tsx
@@ -1,70 +1,10 @@
 "use client";
 import { motion, useTransform, useScroll } from "framer-motion";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useIsClient } from "./IsClientCtx";
 
-const Example = () => {
-  return (
-    <div className="bg-neutral-800">
-      <div className="flex h-48 items-center justify-center">
-        <span className="font-semibold uppercase text-neutral-500">
-          Scroll down
-        </span>
-      </div>
-      <HorizontalScrollCarousel />
-      <div className="flex h-48 items-center justify-center">
-        <span className="font-semibold uppercase text-neutral-500">
-          Scroll up
-        </span>
-      </div>
-    </div>
-  );
-};
-
-const HorizontalScrollCarousel = () => {
-  const targetRef = useRef<HTMLDivElement | null>(null);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
-
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
-
-  return (
-    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
-      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex gap-4">
-          {cards.map((card) => {
-            return <Card card={card} key={card.id} />;
-          })}
-        </motion.div>
-      </div>
-    </section>
-  );
-};
-
-const Card = ({ card }: { card: CardType }) => {
-  return (
-    <div
-      key={card.id}
-      className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
-    >
-      <div
-        style={{
-          backgroundImage: `url(${card.url})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
-      ></div>
-      <div className="absolute inset-0 z-10 grid place-content-center">
-        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
-          {card.title}
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default Example;
+// width of the card in px
+const CARD_WIDTH = 450;
 
 type CardType = {
   url: string;
@@ -109,3 +49,100 @@ const cards: CardType[] = [
     id: 7,
   },
 ];
+
+const HorizontalScrollCarousel = ({
+  direction,
+}: {
+  direction: "left" | "right";
+}) => {
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+  const motionRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+  });
+
+  const [width, setWidth] = useState<number>(1850);
+  // motion.div range settings state
+  const [motionRange, setMotionRange] = useState({
+    start: "1%",
+    end: "-47.5%",
+  });
+  const isClient = useIsClient();
+  const isMobile = width <= CARD_WIDTH;
+
+  useEffect(() => {
+    // getting window width
+    if (isClient) {
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, [isClient]);
+
+  // adjusting motion.div settings for different screen size
+  useEffect(() => {
+    const start = isMobile ? "0%" : "1%";
+    const end = motionRef.current
+      ? `${
+          (width / motionRef.current.scrollWidth) * 100 - (isMobile ? 100 : 101)
+        }%`
+      : "-47.5%";
+    setMotionRange({ start, end });
+  }, [width, isMobile]);
+
+  // transforming Y scroll to X scroll
+  const x = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [
+      direction === "right" ? motionRange.end : motionRange.start,
+      direction === "right" ? motionRange.start : motionRange.end,
+    ]
+  );
+
+  return (
+    <section className="relative h-[300vh] bg-neutral-900" ref={sectionRef}>
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div
+          style={{ x }}
+          className={isMobile ? "flex gap-1" : "flex gap-4"}
+          ref={motionRef}
+        >
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+const Card = ({ card }: { card: CardType }) => {
+  return (
+    <div
+      key={card.id}
+      className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
+      // className={`group relative h-[${CARD_WIDTH}px] w-[${CARD_WIDTH}px] overflow-hidden bg-neutral-200`}
+    >
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+      <div className="absolute inset-0 z-10 grid place-content-center">
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+          {card.title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalScrollCarousel;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Example from "./component/HorizontalScrollCarousel";
+import HorizontalScrollCarousel from "./component/HorizontalScrollCarousel";
+import { IsClientCtxProvider } from "./component/IsClientCtx";
 
 export default function Home() {
   return (
     <main className="">
-      <Example />
+      <div className="bg-neutral-800">
+        <div className="flex h-48 items-center justify-center">
+          <span className="font-semibold uppercase text-neutral-500">
+            Scroll down
+          </span>
+        </div>
+        <IsClientCtxProvider>
+          <HorizontalScrollCarousel direction="left" />
+          <HorizontalScrollCarousel direction="right" />
+        </IsClientCtxProvider>
+        <div className="flex h-48 items-center justify-center">
+          <span className="font-semibold uppercase text-neutral-500">
+            Scroll up
+          </span>
+        </div>
+      </div>
     </main>
   );
 }
